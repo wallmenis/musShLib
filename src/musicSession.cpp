db3a9c6343d8917528554b303557d2dcd004fd3d
Dimosthenis Krallis <dimosthenis.krallis@outlook.com>
2026-02-21 00:01:21 +0200

    Added some extra checks.

--- src/musicSession.cpp
+++ src/musicSession.cpp
@@ -49,7 +49,7 @@ int MusicSession::connectToPeer(std::string peerId)
     }
     while(!ws->isOpen());
 
-    auto pc = pcs[peerId] = std::make_shared<rtc::PeerConnection>();
+    auto pc = pcs[peerId] = std::make_shared<rtc::PeerConnection>(config);
     pc->onLocalDescription([this, peerId](rtc::Description desc){
         nlohmann::json msg = {
             {"id",        peerId},
@@ -71,6 +71,17 @@ int MusicSession::connectToPeer(std::string peerId)
 
     auto dc = dcs[peerId] = pc->createDataChannel("music");
 
+    dc->onMessage([this,dc](rtc::message_variant msg){
+        if(std::holds_alternative<std::string>(msg))
+        {
+            std::cout << std::get<std::string>(msg) << std::endl;
+            if( interperate(std::get<std::string>(msg),dc))
+            {
+                std::cerr << "failed to interperate\n";
+            }
+        }
+    });
+
     pc->onGatheringStateChange([](rtc::PeerConnection::GatheringState state){
         std::cout << "gathering state: " << state << std::endl;
     });
@@ -89,8 +100,10 @@ int MusicSession::forcePullSessionAndPlaylist()
 {
     for (auto i : dcs)
     {
-        while(!i.second->isOpen());
-        i.second->send("test");
+        if(i.second->isOpen())
+            i.second->send("test");
+        else
+            return 1;
     }
     return 0;
 }
@@ -133,6 +146,10 @@ void MusicSession::handleSignallingServer(rtc::message_variant data)
     std::string id = "";
     auto test_type = dat.find("type");
     std::string type = "";
+    auto test_description = dat.find("description");
+    std::string description = "";
+    auto test_candidate = dat.find("candidate");
+    std::string candidate = "";
     if (test_id==dat.end())
     {
         return;
@@ -140,59 +157,47 @@ void MusicSession::handleSignallingServer(rtc::message_variant data)
     id=test_id->get<std::string>();
     std::shared_ptr<rtc::PeerConnection> pc;
     if (pcs.find(id) == pcs.end())
+    {
         pc = pcs[id] = std::make_shared<rtc::PeerConnection>(config); // create if not existant yet
+        pc->onLocalDescription([this, id](rtc::Description desc){
+            nlohmann::json msg = {
+                {"id",        id},
+                {"type",      desc.typeString()},
+                {"description", std::string(desc)}
+            };
+            ws->send(msg.dump());
+        });
+
+        pc->onLocalCandidate([this, id](rtc::Candidate candid){
+            nlohmann::json msg = {
+                {"id",        id},
+                {"type",      "candidate"},
+                {"candidate", std::string(candid)}//,
+                //{"mid",       candid.mid()}
+            };
+            ws->send(msg.dump());
+        });
+    }
     else
         pc = pcs[id];
-    pc->onLocalDescription([this, id](rtc::Description desc){
-        nlohmann::json msg = {
-            {"id",        id},
-            {"type",      desc.typeString()},
-            {"description", std::string(desc)}
-        };
-        ws->send(msg.dump());
-    });
 
-    pc->onLocalCandidate([this, id](rtc::Candidate candid){
-        nlohmann::json msg = {
-            {"id",        id},
-            {"type",      "candidate"},
-            {"candidate", std::string(candid)},
-            {"mid",       candid.mid()}
-        };
-        ws->send(msg.dump());
-    });
     pc->onGatheringStateChange([](rtc::PeerConnection::GatheringState state){
         std::cout << "gathering state: " << state << std::endl;
     });
     pc->onStateChange([](rtc::PeerConnection::State state){
         std::cout << "state: " << state << std::endl;
     });
-    if(test_type==dat.end())
-    {
-        return;
-    }
-
-    type = test_type->get<std::string>();
-
-    std::cout << "added " << type << std::endl;
-    if (type == "answer" || type == "offer") {                  // whether offerer or answerer
-        std::string sdp = dat["description"].get<std::string>();
-        pc->setRemoteDescription(rtc::Description(sdp, type));
-    }
-    if (type == "candidate") {
-        std::string candid = dat["candidate"].get<std::string>();
-        pc->addRemoteCandidate(rtc::Candidate(candid));
-    }
-
-
 
     pc->onDataChannel([this,id](std::shared_ptr<rtc::DataChannel> dc){
-        std::cout << "datachannel made!!!!!!!!!!!!!!!!!!!!" << std::endl;
+        //std::cout << "datachannel made!!!!!!!!!!!!!!!!!!!!" << std::endl;
         dcs[id] = dc;
         dc->onMessage([this,dc](rtc::message_variant msg){
             if(std::holds_alternative<std::string>(msg))
             {
                 std::cout << std::get<std::string>(msg) << std::endl;
+
+                //while(!dc->isOpen());
+                //dc->send("test");
                 if( interperate(std::get<std::string>(msg),dc))
                 {
                     std::cerr << "failed to interperate\n";
@@ -201,15 +206,60 @@ void MusicSession::handleSignallingServer(rtc::message_variant data)
         });
     });
 
+
+    if(test_type==dat.end())
+    {
+        return;
+    }
+    type = test_type->get<std::string>();
+
+    std::cout << "added " << type << std::endl;
+    if (type == "answer" || type == "offer") {                  // whether offerer or answerer
+        if(test_description != dat.end())
+        {
+            description = test_description->get<std::string>();
+            pc->setRemoteDescription(rtc::Description(description, type));
+        }
+    }
+    if (type == "candidate") {
+        if(test_candidate != dat.end())
+        {
+            candidate = test_candidate->get<std::string>();
+            pc->addRemoteCandidate(rtc::Candidate(candidate));
+        }
+    }
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int MusicSession::interperate(std::string message, std::shared_ptr<rtc::DataChannel> dc)
 {
+    if (!dc->isOpen())
+    {
+        return 1;
+    }
+
+    if(!nlohmann::json::accept(message))
+    {
+        std::cerr << "Error parsing, ignoring\n";
+        return 1;
+    }
+
+    auto msg = nlohmann::json::parse(message);
+
+
 
     return 0;
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 MusicSession::~MusicSession()
 {
+    for (auto i : dcs)
+    {
+        i.second->close();
+    }
+    for (auto i : pcs)
+    {
+        i.second->close();
+    }
     dcs.clear();
     pcs.clear();
 }
